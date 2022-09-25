9e61d8db02e91fdb9065a7ca571471339793ee91
Mikael Strid <mikael.strid@vasttrafik.se>
2022-09-25 13:52:11 +0200

    Added header and footer.

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,12 +1,39 @@
 import React from "react";
-import { Container } from "reactstrap";
+import {
+  Container,
+  Nav,
+  Navbar,
+  NavbarBrand,
+  NavItem,
+  NavLink,
+} from "reactstrap";
 import { Outlet } from "react-router-dom";
 
 function Root() {
   return (
-    <Container>
-      <Outlet />
-    </Container>
+    <>
+      <Navbar color="light" className="mb-5">
+        <NavbarBrand href="/">KSK Team 11 Game Planner</NavbarBrand>
+        <Nav>
+          <NavItem>
+            <NavLink href="/players">Spelare</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="/games">Matcher</NavLink>
+          </NavItem>
+        </Nav>
+      </Navbar>
+      <Container>
+        <Outlet />
+      </Container>
+      <div className="container">
+        <footer className="my-4">
+          <p className="text-center text-muted border-top mt-3 pt-3">
+            © 2022 Kållered Icebears Team 11
+          </p>
+        </footer>
+      </div>
+    </>
   );
 }
 
