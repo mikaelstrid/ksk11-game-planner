5776ca30941b486d14845fc444c41d21740d6090
Mikael Strid <mikael.strid@vasttrafik.se>
2022-09-25 16:32:24 +0200

    Basic game page.

--- src/routes/Game.tsx
+++ src/routes/Game.tsx
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { IActivities } from "../models/all.model";
+import { DataService } from "../services/data.service";
 
 function Game() {
+  const [activities, setActivities] = useState<IActivities>();
+
+  useEffect(() => {
+    setActivities(DataService.loadActivities());
+  }, []);
+  let { id } = useParams();
+
+  if (!activities) return <p>Laddar...</p>;
+
+  if (!id) return <p>404</p>;
+
+  const game = activities.games.find((a) => a.id === parseInt(id ?? ""));
+  if (!game) return <p>404</p>;
+
   return (
     <div>
-      <h1>Game</h1>
+      <h2>
+        {`${game.datetime.toLocaleDateString("sv")} ${game.opponent.name}`}
+      </h2>
     </div>
   );
 }
